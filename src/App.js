459e469acfd0697560b7a7d128c67e639b2ef79f
04449819 <kiennvph18558@fpt.edu.vn>
2024-05-22 09:28:24 +0700

    form don gian

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import MyComponent from "./Component/MyComponent";
-function App() {
-  return (
-    <div className="App">
-      {/* <header className="App-header"> */}
-      <MyComponent />
-      {/* </header> */}
-    </div>
-  );
+import React from "react";
+class App extends React.Component {
+  render() {
+    return <MyComponent />;
+  }
 }
-
 export default App;
---
+++ src/Component/UseInfor.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class UserInfor extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>i'm children</h1>
+      </>
+    );
+  }
+}
+
+export default UserInfor;
